3892006521a01de564aa4bb9ed74b833aabbf0b7
Yash <yks.nit@gmail.com>
2018-04-04 05:02:18 +0530

    Master branch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +13,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Testing react application.
         </p>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Link to={"/app"}>
+        It is a link
+      </Link>
+      <Route exact path={"/app"} component={App} />
+      <Route exact path={"/"} component={() => <div>yash</div>} />
+    </div>
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
